a4688e7f649ac2f642a0d04c1fb776c8f8007a50
Hieu Dang <76166385+hieubin218@users.noreply.github.com>
2021-07-12 22:41:20 +0700

    Update QuestionB.js

--- QuestionB.js
+++ QuestionB.js
@@ -1,36 +1,34 @@
-let input = document.getElementById("NumInput");
+let UserInput = document.getElementById("NumInput");
 let statement = document.getElementById("result");
 let clickedButton = document.getElementById("button");
 
-let BooleanValue = false;
+
 
 
 
 function GuessNumber() {
-    let inputValue = input.target.value;
+    let inputValue = UserInput.value;
     let RandomNumber = Math.floor(Math.random() * 10) + 1;
 
-    if (inputValue < 1 && inputValue > 10) {
-        prompt("Invalid Input: Only Number from 1 to 10. Try Again!");
 
-    } if (ParseIn(inputValue) != RandomNumber) {
-        for (i = 0; i < 4; i++) {
-            prompt("Invalid Input: Try Again! You only have 3 times to guess");
-            statement = RandomNumber.innerText;
-            inputValue = input.value;
-        }
 
-        // } else {
-        //     prompt("You chance is over! Sorry!");
-        //     break;
-        } 
+    if (inputValue < 1 || inputValue > 10) {
+        statement.innerHTML = "Invalid Input: Only Number from 1 to 10. Try Again!";
 
-    } if (ParseIn(inputValue) === RandomNumber) {
-        BooleanValue = true;
-        prompt("WOW! Congrats you win!");
+    } else if (inputValue != RandomNumber) {
+        statement.innerHTML = "Upps! Your Guess is Wrong! Lucky Number is " + RandomNumber;
+
+    } else if (inputValue == RandomNumber) {
+
+        statement.innerHTML = "WOW! Congrats! You Won $1000 Special Prize ! Lucky Number is " + RandomNumber;
+
+    } if (isNaN(inputValue)) {
+        statement.innerHTML = "Invalid Input! Please Try Again!";
     }
-   
+
 }
+
+
 
 clickedButton.addEventListener('click', GuessNumber);
 
